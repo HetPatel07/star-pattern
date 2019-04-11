6933ca8dfe1619c072c18a72cdb402f1f0e6718b
hetpatel07 <hets7878401023@gmail.com>
2019-04-11 16:20:17 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 var i = 0;
+//
+//for (i = 0; i < 3; i++) {
+//    for (j = 0; j <= i; j++) {
+//        var span = document.createElement('span');
+//
+//        document.getElementsByClassName('stars')[0].appendChild(span);
+//
+//        document.getElementsByTagName('span')[j].innerHTML = '<i class="star fas fa-star"></i>';
+//        console.log(document.getElementsByClassName('stars')[0].lastChild);
+//
+//        if (j == i) {
+//            document.getElementsByTagName('span')[i] = '<i class="star fas fa-star"></i><br />';
+//        }
+//    }
+//
+//
+//}
+//
 
-for (i = 0; i < 3; i++) {
+
+for (i = 0; i < 5; i++) {
     for (j = 0; j <= i; j++) {
-        var span = document.createElement('span');
-
-        document.getElementsByClassName('stars')[0].appendChild(span);
-
-        document.getElementsByTagName('span')[j].innerHTML = '<i class="star fas fa-star"></i>';
-        console.log(document.getElementsByClassName('stars')[0].lastChild);
-
-        if (j == i) {
-            document.getElementsByTagName('span')[i] = '<i class="star fas fa-star"></i><br />';
-        }
+        document.write('<i class="star fas fa-star"></i>');
     }
-
-
+    document.write('<br>');
 }
